786a6022263fc761e6ec887da2d1826445467381
unknown <giatjjohan@gmail.com>
2026-06-17 10:31:42 +0700

    [Tahap 3] Membuat koneksi database dan abstract class Pendaftaran

---
+++ database.php
@@ -0,0 +1,27 @@
+<?php
+// database.php
+
+class Database {
+    private $host = "localhost";
+    private $db_name = "db_simulasi_pbo_ti1c_giatjohannasrulloh"; 
+    private $password = ""; 
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name, 
+                $this->username, 
+                $this->password
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $exception) {
+            echo "Koneksi database gagal: " . $exception->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
+?>
